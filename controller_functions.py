a6664b6b1b55568401c014fbb83e9dba6e5ff5ae
Donika Tomiqi <donikatomiqi@gmail.com>
2020-02-29 15:49:39 +0100

    check, uncheck and delete ToDo

--- controller_functions.py
+++ controller_functions.py
@@ -6,12 +6,11 @@ from datetime import datetime
 app.secret_key = 'secret'
 bcrypt = Bcrypt(app)
 
-from sqlalchemy import text
+from sqlalchemy import text, func
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
-def landing_page():
-   
+def landing_page():  
     return render_template("index.html")
 
 def register():
@@ -61,10 +60,6 @@ def logout():
     session.clear()
     return redirect('/')
 
-def my_expenses():
-    logged_in_user = User.query.filter_by(id=session['user_id']).first_or_404("Not logged in")
-    expenses = UserExpense.query.filter(UserExpense.user_id==session['user_id']).all()
-    return render_template("user_expenses.html", expenses=expenses, logged_in_user=logged_in_user)
 
 def create_expense():
     name = request.form.get("expense_name")
@@ -80,14 +75,12 @@ def viewAllExpences():
     expenses = UserExpense.query.filter(UserExpense.user_id == session['user_id']).all()
     total =0
     today_date = datetime.now()
-
     for ex in expenses:
         total+= ex.amount
-
     return render_template('viewAll.html', expenses = expenses, total = total, selected = str(today_date.month))
+
 def editExpense(id):
     expense = UserExpense.query.filter_by(id = id).first()
-
     return render_template('editExpense.html', expense = expense)
 
 def editExpenseForm():
@@ -97,8 +90,8 @@ def editExpenseForm():
     expense.amount = request.form['expense_price']
     print(expense)
     db.session.commit()
-
     return redirect('/view_all')
+
 def deleteExpense(ex_id):
     logged_in_user = User.query.filter_by(id=session['user_id']).first()
     expenses = logged_in_user.user_expenses
@@ -106,6 +99,7 @@ def deleteExpense(ex_id):
     expenses.remove(expense)
     db.session.commit()
     return redirect('/view_all')
+
 def filterExpense():
     logged_in_user = User.query.filter_by(id=session['user_id']).first()
     expenses = logged_in_user.user_expenses
@@ -123,5 +117,47 @@ def filterExpense():
     print(selected)
     return render_template('viewAll.html', expenses = expenses_of_this_month, selected =  selected)
 
-def toDo():
-    return render_template('toDoList.html')
+def todo():
+    categories = Category.query.all()
+    all_todos = UserTodo.query.filter(UserTodo.user_id==session['user_id']).all()
+    return render_template('toDoList.html', categories=categories, all_todos=all_todos)
+
+def create_todo():
+    category_id = request.form.get("category")
+    amount = request.form.get("expense_price")
+    content = request.form.get("expense_name")
+    new_todo = UserTodo(user_id=session['user_id'], category_id=category_id, amount=amount, content=content, is_done=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect("/toDoList")
+
+def checkToDo(todo_id):
+    todo = UserTodo.query.filter_by(id = todo_id).first()
+    todo.is_done = True
+    # totalList = db.session.query(UserExpense.amount, func.sum(UserExpense.amount)).group_by(UserExpense.amount).all()
+    # total = UserExpense.query.with_entities(func.sum(UserExpense.amount)).first()
+    
+    name = todo.content
+    amount = todo.amount
+    category_id = todo.category_id
+    new_expense = UserExpense(user_id=session['user_id'], todo_id=todo_id, category_id=category_id, amount=amount, content=name)
+    db.session.add(new_expense)
+    db.session.commit()
+    return redirect('/toDoList')
+
+def uncheckToDo(todo_id):
+    todo = UserTodo.query.filter_by(id = todo_id).first()
+    todo.is_done = False
+    expense = UserExpense.query.filter_by(todo_id=todo_id).first()
+    db.session.delete(expense)
+    db.session.commit()
+    return redirect('/toDoList')
+
+def destroyToDo(todo_id):
+    logged_in_user = User.query.filter_by(id=session['user_id']).first()
+    todos = logged_in_user.user_todos
+    todo = UserTodo.query.filter_by(id = todo_id).first()
+    todos.remove(todo)
+    db.session.commit()
+    return redirect('/toDoList')
+    
--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from config import app
 from controller_functions import (landing_page, register, login, on_register, on_login, logout,
-home, my_expenses, create_expense, viewAllExpences,filterExpense,editExpense,deleteExpense,editExpenseForm,toDo)
+home, create_expense, viewAllExpences, filterExpense, editExpense, deleteExpense, editExpenseForm,
+todo, create_todo, checkToDo, uncheckToDo, destroyToDo)
 
 app.add_url_rule("/", view_func=landing_page)
 app.add_url_rule("/register", view_func=register)
@@ -10,14 +11,17 @@ app.add_url_rule('/home', view_func = home)
 app.add_url_rule("/register_user", view_func=on_register, methods=['POST'])
 app.add_url_rule("/login_user", view_func=on_login, methods=['POST'])
 app.add_url_rule("/logout", view_func=logout)
-app.add_url_rule("/my-expenses", view_func=my_expenses)
 app.add_url_rule("/create_expense", view_func=create_expense, methods=['POST'])
 app.add_url_rule("/view_all", view_func=viewAllExpences)
 app.add_url_rule("/edit/<id>", view_func=editExpense)
-app.add_url_rule("/edit-expense", view_func=editExpenseForm, methods = ['POST'])
+app.add_url_rule("/edit-expense", view_func=editExpenseForm, methods=['POST'])
 app.add_url_rule("/delete/<ex_id>", view_func=deleteExpense)
-app.add_url_rule("/filter-expense", view_func=filterExpense, methods = ['POST'])
-app.add_url_rule("/toDoList", view_func=toDo)
+app.add_url_rule("/filter-expense", view_func=filterExpense, methods=['POST'])
+app.add_url_rule("/toDoList", view_func=todo)
+app.add_url_rule("/create-todo", view_func=create_todo, methods=['POST'])
+app.add_url_rule("/check-todo/<int:todo_id>", view_func=checkToDo)
+app.add_url_rule("/uncheck-todo/<int:todo_id>", view_func=uncheckToDo)
+app.add_url_rule("/destroy-todo/<int:todo_id>", view_func=destroyToDo)
 
 
 
